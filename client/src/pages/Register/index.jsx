1ddecc3f5ec718e62c5d3dd92fa9b1f6462dcabf
Aysun Ismayilova <ismailovaaysuna28@gmail.com>
2025-02-11 19:19:26 +0400

    Add profile image preview feature

--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -1,6 +1,25 @@
 import styles from "../../assets/styles/register.module.scss";
+import { useState } from "react";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    profileImage: null,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, files, type } = e.target;
+    setFormData({
+      ...formData,
+    [name]: type === "file" ? files[0] : value,
+    });
+  };
+
+  console.log(formData)
   
   return (
     <>
@@ -10,28 +29,38 @@ const Register = () => {
           <input
             placeholder="First Name"
             name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
             required
           />
           <input
             placeholder="Last Name"
             name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
             required
           />
           <input
             placeholder="Email"
             name="email"
             type="email"
+            value={formData.email}
+            onChange={handleChange}
             required
           />
           <input
             placeholder="Password"
             name="password"
+            value={formData.password}
+            onChange={handleChange}
             type="password"
             required
           />
           <input
             placeholder="Confirm Password"
             name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
             type="password"
             required
           />
@@ -41,12 +70,22 @@ const Register = () => {
             name="profileImage"
             accept="image/*"
             style={{ display: "none" }}
+            onChange={handleChange}
             required
           />
           <label htmlFor="image">
             <img src="/images/uploadPhoto.png" alt="add profile photo" />
             <p>Upload Your Photo</p>
           </label>
+
+          {formData.profileImage && (
+            <img
+              src={URL.createObjectURL(formData.profileImage)}
+              alt="profile photo"
+              style={{ maxWidth: "80px" }}
+            />
+          )}
+
           <button type="submit">REGISTER</button>
         </form>
         <a href="/login">Already have an account? Log In Here</a>
