fd623e42fe4f471ada003f3efe275219f007b686
Aysun Ismayilova <ismailovaaysuna28@gmail.com>
2025-02-13 04:37:44 +0400

    initial login ui is ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,10 +11,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
         </Route>
           <Route path="*" element={<NotFound />} />
       </Routes>
--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,6 +1,27 @@
+import styles from "../../assets/styles/login.module.scss";
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div className={styles["login"]}>
+      <div className={styles["login_content"]}>
+        <form className={styles["login_content_form"]}>
+          <input
+            type="email"
+            placeholder="Email"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <button type="submit">LOG IN</button>
+        </form>
+        <a href="/register">Don't have an account? Sign In Here</a>
+      </div>
+    </div>
+    </>
   )
 }
 
